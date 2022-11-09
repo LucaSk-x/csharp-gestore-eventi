88c42fdd05aa8b6a443604cd9a48e8111d1537a1
LucaSk-x <lucapiccinno64@gmail.com>
2022-11-09 14:11:05 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Evento
+{
+    public string titolo { get; set; }
+    public DateTime data { get; set; }
+    public int postiMassima { get; }
+    public int posti { get; }
+
+    public Evento (string Titolo, DateTime Data, int Posti)
+    {
+        Titolo = titolo;
+        Data = data;
+        Posti = posti;
+    }
+
+    public string TitoloSetter(string Titolo)
+    {
+        if (Titolo.Lenght <= 0)
+    }
+}
